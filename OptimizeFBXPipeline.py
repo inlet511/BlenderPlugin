0a6e919e26afc0bebca0bb89cf3a15dad7cb7189
AnNing <80669349@qq.com>
2024-04-18 17:23:13 +0800

    添加了对选择物体减面、获取选择物体的顶点/体积比

--- OptimizeFBXPipeline.py
+++ OptimizeFBXPipeline.py
@@ -14,9 +14,9 @@ import bpy
 import bmesh
 
 # 创建一个自定义面板类
-class CustomPanel(bpy.types.Panel):
+class OBJECT_PT_CustomPanel(bpy.types.Panel):
     bl_label = "优化模型"  # 面板的名称
-    bl_idname = "PT_CustomPanel"  # 面板的唯一标识符
+    bl_idname = "OBJECT_PT_CustomPanel"  # 面板的唯一标识符
     bl_space_type = 'VIEW_3D'  # 面板所在区域
     bl_region_type = 'UI'  # 面板所在区域的类型
     bl_category = "优化流程"  # 面板所在选项卡的名称
@@ -27,16 +27,17 @@ class CustomPanel(bpy.types.Panel):
         scene = context.scene
 
         layout.operator("scene.clear_scene",text = "清空场景")
-        layout.operator("scene.import_fbx", text="导入FBX")
         layout.operator("object.delete_loose_element", text="删除游离元素")
         layout.operator("object.dissolve_degenerated", text="融并清理")        
         layout.operator("object.merge_by_distance", text="按间距合并")
         layout.operator("object.fill_holes", text="填补漏洞")
-        layout.operator("object.decimate", text="减面")
+        layout.operator("object.decimateselected", text="选择对象减面")
+        layout.operator("object.get_vertex_volume_rate", text="获取对象顶点体积比")
+        layout.operator("object.decimate", text="整体减面")
         layout.operator("scene.export_fbx", text="导出FBX")
 
 # 清空场景
-class EmptyScene(bpy.types.Operator):
+class OBJECT_OT_EmptyScene(bpy.types.Operator):
     bl_idname = "scene.clear_scene"
     bl_label = "清空场景"
     bl_description = "清空整个场景，开始时进行操作"
@@ -55,7 +56,7 @@ class EmptyScene(bpy.types.Operator):
 
 
 # 导入FBX
-class ImportFBXOperator(bpy.types.Operator):
+class OBJECT_OT_ImportFBXOperator(bpy.types.Operator):
     bl_idname = "scene.import_fbx"
     bl_label = "导入FBX场景"
     bl_description = "导入FBX文件"
@@ -65,7 +66,7 @@ class ImportFBXOperator(bpy.types.Operator):
         return {'FINISHED'}
 
 # 导出FBX
-class ExportFBXOperator(bpy.types.Operator):
+class OBJECT_OT_ExportFBXOperator(bpy.types.Operator):
     bl_idname = "scene.export_fbx"
     bl_label = "导出FBX场景"
     bl_description = "导出FBX文件"
@@ -75,7 +76,7 @@ class ExportFBXOperator(bpy.types.Operator):
         return {'FINISHED'}
 
 # 删除游离元素
-class DeleteLooseOperator(bpy.types.Operator):
+class OBJECT_OT_DeleteLooseOperator(bpy.types.Operator):
     bl_idname = "object.delete_loose_element"
     bl_label = "删除游离元素"
     bl_description = "删除游离的点、边"
@@ -97,7 +98,7 @@ class DeleteLooseOperator(bpy.types.Operator):
 
 
 # 融合面积为零的面以及长度为零的边
-class DissolveDegeneratedOperator(bpy.types.Operator):
+class OBJECT_OT_DissolveDegeneratedOperator(bpy.types.Operator):
     bl_idname = "object.dissolve_degenerated"
     bl_label = "融并清理"
     bl_description = "清理面积为零的面以及长度为零的边"
@@ -119,7 +120,7 @@ class DissolveDegeneratedOperator(bpy.types.Operator):
         return {'FINISHED'}
 
 # 按间距合并
-class MergeByDistanceOperator(bpy.types.Operator):
+class OBJECT_OT_MergeByDistanceOperator(bpy.types.Operator):
     bl_idname = "object.merge_by_distance"
     bl_label = "按间距合并"
     bl_description = "合并重合的点(按照距离)"
@@ -147,7 +148,7 @@ class MergeByDistanceOperator(bpy.types.Operator):
         return context.window_manager.invoke_props_dialog(self)
 
 # 填补漏洞
-class FillHoleOperator(bpy.types.Operator):
+class OBJECT_OT_FillHoleOperator(bpy.types.Operator):
     bl_idname = "object.fill_holes"
     bl_label = "填补漏洞"
     bl_description = "填补破开的洞"
@@ -175,20 +176,21 @@ class FillHoleOperator(bpy.types.Operator):
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
 
-# 定义一个操作符用于添加 "Decimate" 修饰器
-class AddDecimateModifierOperator(bpy.types.Operator):
-    bl_idname = "object.decimate"
+class OBJECT_OT_DecimateSelectedOperator(bpy.types.Operator):
+    bl_idname = "object.decimateselected"
     bl_label = "减面设置"
-    bl_description = "给对象添加减面修改器"
-
-    ratio : bpy.props.FloatProperty(name="精简比例",default=0.3)
+    bl_description = "给所选对象减面"
+    
+    ratio : bpy.props.FloatProperty(name="精简到比例",default=0.3)
     
     # 执行操作的函数
     def execute(self, context):
         r = self.ratio
+        
         # 获取当前选中的对象
         meshes = set(o for o in context.selected_objects
                       if o.type == 'MESH')
+        
         for m in meshes:
             bpy.context.view_layer.objects.active = m
             bpy.ops.object.mode_set(mode='OBJECT')  # 确保处于对象模式
@@ -201,6 +203,89 @@ class AddDecimateModifierOperator(bpy.types.Operator):
             bpy.ops.mesh.decimate(ratio=r)  # 在这里设置减面比率
 
             bpy.ops.object.mode_set(mode='OBJECT')  # 返回对象模式
+            
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+
+
+class OBJECT_OT_GetVertexVolumeRateOperator(bpy.types.Operator):
+    bl_idname = "object.get_vertex_volume_rate"
+    bl_label = "获取顶点体积比"
+    bl_description = "获取所选对象的顶点体积比"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Tools"
+    
+    message = bpy.props.StringProperty(name="Message", default="")
+    
+    def execute(self, context):
+         # 获取当前选中的对象
+        meshes = [o for o in context.selected_objects if o.type == 'MESH']
+        if len(meshes) == 0:
+            self.report({'INFO'}, "请选择一个模型")            
+        else:        
+            obj = meshes[0]
+            vertices_count = len(obj.data.vertices)
+            bbox_volume = obj.dimensions[0] * obj.dimensions[1] * obj.dimensions[2]
+            
+            ratio = vertices_count / bbox_volume
+            
+            self.message = "顶点数: %d, 体积: %f, 顶点体积比: %f" %(vertices_count, bbox_volume, ratio)
+            
+            self.report({'INFO'},self.message)
+        return {'FINISHED'}        
+
+
+# 定义一个操作符用于添加 Decimate修饰器
+class OBJECT_OT_DecimateAll(bpy.types.Operator):
+    bl_idname = "object.decimate"
+    bl_label = "减面设置"
+    bl_description = "给对象添加减面修改器"
+
+    vertex_volume_ratio: bpy.props.FloatProperty(name="顶点体积比", default=5e9)
+    ratio : bpy.props.FloatProperty(name="精简到比例",default=0.3)
+    
+    # 执行操作的函数
+    def execute(self, context):
+        vvr = self.vertex_volume_ratio
+        r = self.ratio
+        
+        objects = bpy.data.objects
+                
+        # 遍历所有对象
+        for obj in objects:
+        # 只考虑是网格的对象
+            if obj.type == 'MESH':
+                # 获取对象的顶点数和边界框体积
+                vertices_count = len(obj.data.vertices)
+                bbox_volume = obj.dimensions[0] * obj.dimensions[1] * obj.dimensions[2]
+                
+                # 计算顶点数和bbox体积的比例
+                if bbox_volume == 0:
+                    self.report({'INFO'}, 'Volume is 0')
+                else:
+                    vertex_to_bbox_ratio = vertices_count / bbox_volume
+                    info_str = str(vertex_to_bbox_ratio)
+                    self.report({'INFO'}, info_str)
+                    # file.write(info_str + '\n')
+                    
+                
+                    # 如果比例大于阈值，则选择该对象                    
+                    if vertex_to_bbox_ratio > vvr:
+                        bpy.context.view_layer.objects.active = obj
+                        bpy.ops.object.mode_set(mode='OBJECT')  # 确保处于对象模式
+                        bpy.ops.object.select_all(action='DESELECT')  # 取消所有选择
+                        obj.select_set(True)  # 选择当前对象
+                        bpy.ops.object.mode_set(mode='EDIT')  # 进入编辑模式
+                        bpy.ops.mesh.select_all(action='SELECT')  # 选择所有面
+
+                        # 使用Decimate进行减面
+                        bpy.ops.mesh.decimate(ratio=r)  # 在这里设置减面比率
+                        bpy.ops.object.mode_set(mode='OBJECT')  # 返回对象模式
+        # file.close()    
         return {'FINISHED'}
     
     def invoke(self, context, event):
@@ -213,28 +298,32 @@ class AddDecimateModifierOperator(bpy.types.Operator):
 
 # 注册面板和操作符
 def register():
-    bpy.utils.register_class(CustomPanel)
-    bpy.utils.register_class(EmptyScene)
-    bpy.utils.register_class(ImportFBXOperator)
-    bpy.utils.register_class(ExportFBXOperator)
-    bpy.utils.register_class(AddDecimateModifierOperator)
-    bpy.utils.register_class(DeleteLooseOperator)
-    bpy.utils.register_class(DissolveDegeneratedOperator)
-    bpy.utils.register_class(MergeByDistanceOperator)
-    bpy.utils.register_class(FillHoleOperator)
+    bpy.utils.register_class(OBJECT_PT_CustomPanel)
+    bpy.utils.register_class(OBJECT_OT_EmptyScene)
+    bpy.utils.register_class(OBJECT_OT_ImportFBXOperator)
+    bpy.utils.register_class(OBJECT_OT_ExportFBXOperator)
+    bpy.utils.register_class(OBJECT_OT_DecimateSelectedOperator)
+    bpy.utils.register_class(OBJECT_OT_GetVertexVolumeRateOperator)
+    bpy.utils.register_class(OBJECT_OT_DecimateAll)
+    bpy.utils.register_class(OBJECT_OT_DeleteLooseOperator)
+    bpy.utils.register_class(OBJECT_OT_DissolveDegeneratedOperator)
+    bpy.utils.register_class(OBJECT_OT_MergeByDistanceOperator)
+    bpy.utils.register_class(OBJECT_OT_FillHoleOperator)
 
 
 # 注销面板和操作符
 def unregister():
-    bpy.utils.unregister_class(CustomPanel)
-    bpy.utils.unregister_class(EmptyScene)
-    bpy.utils.unregister_class(ImportFBXOperator)
-    bpy.utils.unregister_class(ExportFBXOperator)
-    bpy.utils.unregister_class(AddDecimateModifierOperator)
-    bpy.utils.unregister_class(DeleteLooseOperator)
-    bpy.utils.unregister_class(DissolveDegeneratedOperator)
-    bpy.utils.unregister_class(MergeByDistanceOperator)
-    bpy.utils.unregister_class(FillHoleOperator)
+    bpy.utils.unregister_class(OBJECT_PT_CustomPanel)
+    bpy.utils.unregister_class(OBJECT_OT_EmptyScene)
+    bpy.utils.unregister_class(OBJECT_OT_ImportFBXOperator)
+    bpy.utils.unregister_class(OBJECT_OT_ExportFBXOperator)
+    bpy.utils.unregister_class(OBJECT_OT_DecimateSelectedOperator)
+    bpy.utils.unregister_class(OBJECT_OT_GetVertexVolumeRateOperator)
+    bpy.utils.unregister_class(OBJECT_OT_DecimateAll)
+    bpy.utils.unregister_class(OBJECT_OT_DeleteLooseOperator)
+    bpy.utils.unregister_class(OBJECT_OT_DissolveDegeneratedOperator)
+    bpy.utils.unregister_class(OBJECT_OT_MergeByDistanceOperator)
+    bpy.utils.unregister_class(OBJECT_OT_FillHoleOperator)
 
 
 
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Blender Plugins(python)
 
 - OptimizeFBXPipeline.py : 导入导出FBX，一键减面，一键补洞
+添加了对选择物体减面、获取选择物体的顶点/体积比
